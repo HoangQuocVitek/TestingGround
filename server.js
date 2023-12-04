b888ecac6987a3694d34054e5791b403240652b8
HoangQuocViet <viethoanquoc@gmail.com>
2023-12-04 13:29:53 +0100

    2.0

--- server.js
+++ server.js
@@ -1,48 +1,78 @@
 const express = require('express');
 const session = require('express-session');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
-
-// Nastavení session middleware
 app.use(session({
   secret: 'tajnasekret',
   resave: false,
   saveUninitialized: true,
 }));
 
-//nastavi session do cookies
-app.get('/set', (req, res) => {
-  req.session.username = 'JohnDoe';
-  res.send('Session data set.');
-});
-//vypise session do dokumentu
-app.get('/get', (req, res) => {
-  const username = req.session.username;
-  res.send('Username from session: ' + username);
-});
-//posle soubor HTML
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public'));
+
+const con = mysql.createConnection({
+  host: '192.168.1.161',
+  user: 'vitek.hoang',
+  password: 'BookOfDarkness',
+  database: 'vitek.hoang',
+  port: 3001
 });
 
-//zajistuje chat
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to MySQL!');
+});
+
 io.on('connection', (socket) => {
-  console.log('Nový uživatel připojen');
+  console.log('New user connected');
 
   socket.on('chat message', (message) => {
-    console.log('Přijata zpráva: ' + message);
+    console.log('Received message: ' + message);
     io.emit('chat message', message);
   });
 
   socket.on('disconnect', () => {
-    console.log('Uživatel odpojen');
+    console.log('User disconnected');
   });
 });
 
+app.post('/vitek.hoang', function (request, response, next) {
+  const { username, email, password, confirmPassword } = request.body;
+
+  if (password !== confirmPassword) {
+    return response.status(400).send('Passwords do not match');
+  }
+
+  const sql = `INSERT INTO userdatabase (username, email, password) VALUES (?, ?, ?)`;
+  con.query(sql, [username, email, password], (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      return response.status(500).send('Error inserting into the database');
+    }
+    console.log('User inserted into the database');
+    response.send('User inserted into the database');
+  });
+});
+
+app.get('/registrace', (req, res) => {
+  res.render('registrace');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
 const port = 80;
-http.listen(port, () => {
-  console.log(`Server běží na adrese http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
